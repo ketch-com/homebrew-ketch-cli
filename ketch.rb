1a9d357064f275b0c969609f853c2d74288aad9c
Seth Yates <29878+sethyates@users.noreply.github.com>
2020-12-17 12:11:48 -0800

    chore: bump

--- ketch.rb
+++ ketch.rb
@@ -1,16 +1,16 @@
 class Ketch < Formula
   desc "Ketch CLI utility"
   homepage "https://ketch.sh"
-  version "0.4.5"
+  version "0.1.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/ketch-com/ketch-cli/releases/download/v0.4.5/darwin-amd64.tgz"
-    sha256 "eabbe61f15ba99eb70145c344c3ee3fb484942f01d58be81ea6f327642a487a1"
+    url "https://github.com/ketch-com/ketch-cli/releases/download/v0.1.2/darwin-amd64.tgz"
+    sha256 "a27f46765c6f84eb85183e886b5252ad3ecc970763ee1cfc40c452c575d95204"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/ketch-com/ketch-cli/releases/download/v0.4.5/linux-amd64.tgz"
-      sha256 "40fe7f215161d42a5a7a3b62bd0c560681fae812f874a8147e5e7b2ec90c9d6a"
+      url "https://github.com/ketch-com/ketch-cli/releases/download/v0.1.2/linux-amd64.tgz"
+      sha256 "24b1613f6285537ac7e2db33a15c181f4e2ba2026a39a2c317d2ed955d566bb1"
     end
   end
 
